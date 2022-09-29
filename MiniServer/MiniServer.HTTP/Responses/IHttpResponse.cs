ba6e4e6dca3587a4a28b1df5f3c01f407c554325
Bojidar <BDDimitrov18@codingburgas.bg>
2022-09-29 10:20:22 +0300

    Page 6 Done

---
+++ MiniServer/MiniServer.HTTP/Responses/IHttpResponse.cs
@@ -0,0 +1,54 @@
+﻿using MiniServer.HTTP.Headers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static MiniServer.HTTP.Enums.Enumerators;
+
+namespace MiniServer.HTTP.Responses
+{
+    public interface IHttpResponse {
+        HttpResponseStatusCode StatusCode { get; set; }
+        IHttpHeaderCollection Headers { get; }
+        byte[] Control { get; set; }
+
+        void AddHeader(HttpHeader header);
+
+        byte[] GetBytes();
+
+    }
+
+    class HttpResponse : IHttpResponse
+    {
+        public HttpResponse()
+        {
+            this.Headers = new HttpHeaderCollection();
+            this.Content = new byte[0];
+        }
+
+        public HttpResponse(HttpResponseStatusCode statusCode) : this() {
+            CodeValidator.ThrowIfNull(StatusCode, name: nameof(StatusCode));
+            this.StatusCode = statusCode;
+        }
+        
+        public HttpResponseStatusCode StatusCode { get; set; }
+
+        public IHttpHeaderCollection Headers { get; }
+
+        public byte[] Control { get; }
+
+        public void AddHeader(HttpHeader header)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetBytes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
